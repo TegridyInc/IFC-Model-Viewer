bbff636f4e0803f1c6ba423afdbe3a3a711b24dd
TegridyInc <cassiemelink@gmail.com>
2025-05-26 10:32:07 +0200

    hiding big models does not cause a freeze

--- src/scripts/Functions/Culler.ts
+++ src/scripts/Functions/Culler.ts
@@ -2,7 +2,8 @@ import * as FRA from '@thatopen/fragments'
 import { world, culler, fragmentHider } from '../Viewer/Components'
 import {IFCDispatcher, IFCModel} from '../Viewer/IFC'
 
-const meshState = new Map<string, boolean>();
+type MeshState = Map<number, boolean>;
+const meshStates = new Map<string, MeshState>();
 
 document.addEventListener('onViewportLoaded', ()=>{
     world.camera.controls.addEventListener("sleep", () => {
@@ -26,29 +27,41 @@ document.addEventListener('onModelAdded', (e:CustomEvent<IFCModel>)=>{
 function UpdateColorMeshVisibility(event: {target: IFCDispatcher}) {
     const model = event.target.ifc;
 
+    var meshState: MeshState;
+    if(meshStates.has(model.uuid)) 
+        meshState = meshStates.get(model.uuid)
+    else {
+        meshState = new Map<number, boolean>();
+        meshStates.set(model.uuid, meshState);
+    }
+
     if(!model.visible) {
-        model.children.forEach(child => {
+        model.children.forEach((child, i) => {
             if(child instanceof FRA.FragmentMesh) {
                 const colorMesh = culler.colorMeshes.get(child.uuid);
                 if (colorMesh != undefined) 
-                    meshState.set(child.uuid, colorMesh.visible);
+                    meshState.set(i, colorMesh.visible);
                 else 
-                    meshState.set(child.uuid, child.visible);
-                
-                fragmentHider.set(false, model.getFragmentMap(child.fragment.ids));
+                    meshState.set(i, child.visible);
             } 
         })
         
-        culler.needsUpdate = true;
+        fragmentHider.set(false, model.getFragmentMap());
     } else {
-        model.children.forEach(child => {
+        fragmentHider.set(true, model.getFragmentMap());
+
+        var ids: number[] = [];
+        model.children.forEach((child, i) => {
             if(child instanceof FRA.FragmentMesh) {
-                fragmentHider.set(meshState.get(child.uuid), model.getFragmentMap(child.fragment.ids));
+                if(!meshState.get(i)) {
+                    ids.push(...child.fragment.ids);
+                }
             } 
         })
-
-        culler.needsUpdate = true;
+        fragmentHider.set(false, model.getFragmentMap(ids));
     }
+    
+    culler.needsUpdate = true;
 }
 
 function UpdateColorMeshPosition(event: {target: IFCDispatcher}) {
